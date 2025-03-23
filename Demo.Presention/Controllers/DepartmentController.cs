5ff49a0bbfba830bf9b191ba03dfb09d2713340e
Mohamed Osamaa <mohammedosama1a2@gmail.com>
2025-03-23 02:15:16 +0200

    Part 06 Department Controller Edit

--- Demo.Presention/Controllers/DepartmentController.cs
+++ Demo.Presention/Controllers/DepartmentController.cs
@@ -1,5 +1,6 @@
 ﻿using Demo.BusinessLogicLayer.DTOS;
 using Demo.BusinessLogicLayer.Services;
+using Demo.Presention.ViewModels.DepartmentViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore.Diagnostics;
 
@@ -62,6 +63,60 @@ namespace Demo.Presention.Controllers
                 return NotFound();
 
             return View(Department);
+        }
+        #endregion
+
+        #region Edit
+
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if (!id.HasValue) return BadRequest();
+            var Department = _departmentServices.GetById(id.Value);
+            if (Department is null) return NotFound();
+            var EditedDepartment = new DepartmentEditViewModel()
+            {
+                Name = Department.Name,
+                Code = Department.Code,
+                Description = Department.Description,
+                CreatedOn = Department.CreatedOn
+            };
+            return View(EditedDepartment);
+        }
+
+        [HttpPost]
+        public IActionResult Edit([FromRoute]int id ,DepartmentEditViewModel viewModel)
+        {
+            if(ModelState.IsValid)
+            {
+                try
+                {
+                    var UpdatedDepartment = new UpdatedDepartmentDTO()
+                    {
+                        ID = id,
+                        Name = viewModel.Name,
+                        Code = viewModel.Code,
+                        Description = viewModel.Description,
+                        CreatedOn = viewModel.CreatedOn,
+                    };
+                    bool result =_departmentServices.UpdateExistedDepartment(UpdatedDepartment);
+                    if (result)
+                        return RedirectToAction(nameof(Index));
+                    else
+                        ModelState.AddModelError(string.Empty, "Department Is not Updated.");
+                }
+                catch (Exception ex)
+                {
+                    if (_environment.IsDevelopment())
+                        ModelState.AddModelError(string.Empty, ex.Message);
+                    else
+                    {
+                        _logger.LogError(ex.Message);
+                        return View("ErrorView",ex.Message);
+                    }
+                }
+            }
+            return View(viewModel);
         }
         #endregion
 
