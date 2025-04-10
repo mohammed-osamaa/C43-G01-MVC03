a1c0e39f37560f6bb0ba857fcde37ae493975063
Mohamed Osamaa <mohammedosama1a2@gmail.com>
2025-04-10 18:34:45 +0200

    Part 07 AuthLayout

---
+++ Demo.Presention/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.Presention.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
